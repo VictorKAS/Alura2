ab322d90d0930c1ff7b136625e85637b85180d80
VictorKAS <victorkaynan21@gmail.com>
2025-08-01 18:57:33 -0300

    att

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,11 @@
-alert ('Olá, mundo!');
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+/* * Adiciona um evento de clique ao botão 
+let botao = document.querySelector('button');
+botao.addEventListener('click', function() {
+    alert('Fui clicado!');
+});  */
