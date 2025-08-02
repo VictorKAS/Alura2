75ee23e275b55faa0849f711befdc8220d83fd08
VictorKAS <victorkaynan21@gmail.com>
2025-08-01 21:17:37 -0300

    att

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,8 +1,31 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+    console.log(contador);
+    if (contador === listaDeTeclas.length) {
+        console.log('Chegou ao final da lista de teclas');
+    }
+    
+}
+
+
+
+// document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 /* * Adiciona um evento de clique ao botão 
 let botao = document.querySelector('button');
