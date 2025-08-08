02a7ae1c9b7d32a68cdb93eabaa72f35d3b4d8b5
VictorKAS <victorkaynan21@gmail.com>
2025-08-08 15:37:11 -0300

    att

--- Javascript/3802-javascript-assincrono-projeto-base/scripts.js
+++ Javascript/3802-javascript-assincrono-projeto-base/scripts.js
@@ -4,3 +4,17 @@ const inputUpload = document.getElementById('image-upload');
 uploadBtn.addEventListener('click', () => {
     inputUpload.click();
 });
+
+document.getElementById('image-upload').addEventListener('change', function(event) {
+    var file = event.target.files[0];
+    // Agora temos o arquivo e podemos fazer mais validações
+    if (!file.type.match('image/png') && !file.type.match('image/jpeg')) {
+        lert('Por favor, selecione uma imagem PNG ou JPEG.');
+        return;
+    }
+    // Vamos limitar o tamanho a 2MB
+    if (file.size > 2 * 1024 * 1024) {
+        alert('A imagem deve ter no máximo 2MB.');
+        return;
+    }
+});
