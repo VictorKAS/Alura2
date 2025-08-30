c889d9a6725174312066e916cc9fa6a241346615
VictorKAS <victorkaynan21@gmail.com>
2025-08-30 14:04:49 -0300

    att

--- Python/projeto alura 1/app.py
+++ Python/projeto alura 1/app.py
@@ -1,11 +1,43 @@
-print("""
+import os
+
+def exibir_nome_do_app():
+    print("""
 █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
 ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█""")
 
-print('1 - Cadastrar restaurante')
-print('2 - Listar restaurante')
-print('3 - Ativar restaurante')    
-print('4 - Sair')
+def exibir_menu():
+    print('1 - Cadastrar restaurante')
+    print('2 - Listar restaurantes')
+    print('3 - Ativar restaurante')    
+    print('4 - Sair')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'você escolheu a opção {opcao_escolhida}')
+def escolher_opcao():
+    # opcao_escolhida = input('Escolha uma opção: ')
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    # opcao_escolhida = int(opcao_escolhida)
+    print(f'você escolheu a opção {opcao_escolhida}')
+
+    def finalizar_app():
+        os.system('cls')
+        print('Finalizando o app\n')
+        
+
+    if opcao_escolhida == 1:
+        print('Cadastrando restaurante')
+
+    elif opcao_escolhida == 2:
+        print('Listando restaurantes')
+
+    elif opcao_escolhida == 3:
+        print('Ativando restaurante')
+
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_app()
+    exibir_menu()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
